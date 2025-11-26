c37073ca057b7bd3b7b606811cc31057b123ab19
Łukasz Gil <lukaszgil7@gmail.com>
2025-11-26 23:32:29 +0100

    WiP

--- scripts/release-win.js
+++ scripts/release-win.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { execSync } from 'child_process';
 
-const versionArg = process.argv[2];
+const versionArg = process.argv[2] ?? "";
 
 function isSemver(version) {
   return /^\d+\.\d+\.\d+$/.test(version);
@@ -9,25 +9,15 @@ function isSemver(version) {
 
 const validBumps = ['patch', 'minor', 'major'];
 
-if (!versionArg) {
-  console.error('❌ Error: you need to provide one of following arguments - patch, minor, major or exact version X.Y.Z');
-  process.exit(1);
-}
+// if (!versionArg) {
+//   console.error('❌ Error: you need to provide one of following arguments - patch, minor, major or exact version X.Y.Z');
+//   process.exit(1);
+// }
 
 if (!validBumps.includes(versionArg) && !isSemver(versionArg)) {
   console.error(`❌ Error: invalid argument "${versionArg}". Allowed: patch, minor, major or X.Y.Z`);
   process.exit(1);
 }
-
-
-// console.log('🔨 Building Windows executable...');
-// try {
-//   execSync('npm run dist:win', { stdio: 'inherit' });
-// } catch (err) {
-//   console.error('❌ Error while building .exe');
-//   process.exit(1);
-// }
-
 
 const releaseCmd = `npx release-it ${versionArg}`;
 console.log(`🚀 Running: ${releaseCmd}`);
