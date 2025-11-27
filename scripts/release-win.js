4b0c4c7351fbab087f2bafbedfdcf55bab831879
Łukasz Gil <lukaszgil7@gmail.com>
2025-11-27 17:23:41 +0100

    fix: release script

--- scripts/release-win.js
+++ scripts/release-win.js
@@ -9,7 +9,7 @@ if (releaseName === '') {
 }
 
 
-const releaseCmd = `npx release-it --github.releaseName=${releaseName} --only-version`;
+const releaseCmd = `npx release-it --github.releaseName="${releaseName}" --only-version`;
 console.log(`🚀 Running: ${releaseCmd}`);
 try {
   execSync(releaseCmd, { stdio: 'inherit' });
