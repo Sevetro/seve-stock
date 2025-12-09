4d500c9e757304502125009c0b4885b6b76e58b6
Łukasz Gil <lukaszgil7@gmail.com>
2025-12-09 17:01:01 +0100

    feat: save permamently data in appData roaming

--- src/electron/stock-data/constants.ts
+++ src/electron/stock-data/constants.ts
@@ -1,1 +1,9 @@
-export const dataCacheDirname = 'data-cache';
+import { app } from 'electron';
+import path from 'path';
+
+import { isDev } from '../utils/core.js';
+
+const userDataPath = app.getPath('userData');
+const dataCache = 'data-cache';
+
+export const dataCacheDirname = isDev() ? dataCache : path.join(userDataPath, dataCache);
